2136ae6e66a0aec312fe00d783f3381f524862ca
bakunts8 <khachikbakunts@gmail.com>
2022-12-24 14:57:50 +0400

    Add Main.java by new variables and .gitignore file

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int c;
+        int a = 8;
+        int b = 12;
+        c = a * a + b * b;
+        System.out.println("c = " + c);
+    }
+}
