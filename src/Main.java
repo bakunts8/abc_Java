857ee64273df07fe062c3abab3dbe6180b0483c0
bakunts8 <khachikbakunts@gmail.com>
2022-12-24 15:00:34 +0400

    calculate the area of the table

--- src/Main.java
+++ src/Main.java
@@ -5,5 +5,11 @@ public class Main {
         int b = 12;
         c = a * a + b * b;
         System.out.println("c = " + c);
+
+        // Table bases and height data are given. Find the area.
+        a = 18;
+        b = 14;
+        c = 10; //height
+        System.out.println("the area of the table is equal " + (a + b) / 2 * c);
     }
 }
