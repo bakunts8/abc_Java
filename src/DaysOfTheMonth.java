3eb1edc062da8fd66122996bc6eddd901f66e940
bakunts8 <khachikbakunts@gmail.com>
2023-01-05 21:47:59 +0400

    Add Triangle and DaysOfTheMonth java files with their functionality

---
+++ src/DaysOfTheMonth.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class DaysOfTheMonth {
+    public static void main(String[] args) {
+        System.out.println("Enter the month number and find out the number of days in that month");
+        System.out.println("input must be in the range 1 to 12 inclusive");
+        int i;
+        int x;
+        Scanner input = new Scanner(System.in);
+        i = input.nextInt();
+        if (i > 12 || i <= 0) {
+            System.out.println("invalid data");
+            System.exit(0);
+        }
+
+        switch (i) {
+            case 1, 3, 5, 7, 8, 10, 12 -> {
+                x = 31;
+                System.out.println(i + " ---this month has " + x + " days");
+            }
+            case 2 -> {
+                x = 28;
+                System.out.println(i + " ---this month has " + x + " or 29 days");
+            }
+
+            case 4, 6, 9, 11 -> {
+                x = 30;
+                System.out.println(i + " ---this month has " + x + " days");
+            }
+        }
+
+
+    }
+}
---
+++ src/Triangle.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+public class Triangle {
+    public static void main(String[] args) {
+
+        Scanner data = new Scanner(System.in);
+
+        System.out.println("Whether a triangle will be drawn with the data you entered");
+
+        System.out.println("input first side");
+        int a = data.nextInt();
+        if (a <= 0) {
+            System.out.println("invalid data, input again");
+            a = data.nextInt();
+            if (a <= 0) {
+                System.exit(0);
+            }
+        }
+
+        System.out.println("input second side");
+        int b = data.nextInt();
+        if (b <= 0) {
+            System.out.println("invalid data, input again");
+            b = data.nextInt();
+            if (b <= 0) {
+                System.exit(0);
+            }
+        }
+
+        System.out.println("input third side");
+        int c = data.nextInt();
+        if (c <= 0) {
+            System.out.println("invalid data, input again");
+            c = data.nextInt();
+            if (c <= 0) {
+                System.exit(0);
+            }
+        }
+
+        if (c < a + b) {
+            System.out.println("Yes it is possible");
+        } else
+            System.out.println("No, that's not possible");
+
+    }
+}
