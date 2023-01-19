9061cc4d6e30312e67181f6003cadb760697bded
bakunts8 <khachikbakunts@gmail.com>
2023-01-19 23:18:02 +0400

    Add  solution :)) in PrimeNumber.java

---
+++ src/PrimeNumber.java
@@ -0,0 +1,25 @@
+public class PrimeNumber {
+    public static void main(String[] args) {
+
+        int[] array = {8, 51, 20, 70, 30, 85};
+        int q = 0;
+        for (int i = 0, a = 0; i < array.length; i++) {
+            for (int x = 2; x <= array[i] / 2; x++) {
+
+                if (array[i] % x == 0) {
+                    a++;
+                    q++;
+                    break;
+                }
+            }
+            if (a == i) {
+                a++;
+                System.out.println(array[i]);
+            }
+        }
+        if (q == array.length) {
+            System.out.println("There is no prime number");
+        }
+    }
+}
+
