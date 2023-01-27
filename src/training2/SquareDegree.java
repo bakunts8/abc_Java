74a13073b5f5c8f8917bfc43564d2cdcbb88de3f
bakunts8 <khachikbakunts@gmail.com>
2023-01-27 17:28:02 +0400

    Add SquareDegree.java in package2

--- src/training2/SquareDegree.java
+++ src/training2/SquareDegree.java
@@ -1,16 +1,19 @@
 
 package training2;
+
 import java.util.Scanner;
+
 public class SquareDegree {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         System.out.println("Which number do you want to square?");
-        int a  = s.nextInt();
+        int a = s.nextInt();
         System.out.println("How many degrees?");
         int b = s.nextInt();
-        int summary = square(a,b);
+        int summary = square(a, b);
         System.out.println("It is " + summary);
     }
+
     static int square(int a, int b) {
 
         if (a > 10 || a < -10 || b > 10 || b < -10) {
