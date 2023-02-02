fef0315a45b5736aa0a7d5becd9ed661facff97c
bakunts8 <khachikbakunts@gmail.com>
2023-02-02 15:22:07 +0400

    Add Armstrong.java file

---
+++ src/training2/Armstrong.java
@@ -0,0 +1,37 @@
+package training2;
+
+public class Armstrong {
+    public static void main(String[] args) {
+        int x = 548834;
+        System.out.println(isArm(x));
+    }
+
+    static boolean isArm(int n) {
+        boolean isArmstrong = false;
+        int a = 10;
+        int sumOfSquareDigits = 0;
+        for (int i = 0; i < numberLength(n); i++) {
+            int digit = (n - n / a * a) / (a / 10);
+            a *= 10;
+            int square = 1;
+
+            for (int j = 0; j < numberLength(n); j++) {
+                square *= digit;
+            }
+
+            sumOfSquareDigits += square;
+        }
+
+        if (sumOfSquareDigits == n) {
+            isArmstrong = true;
+        }
+        return isArmstrong;
+    }
+    static int numberLength(int n) {
+        int length = 0;
+        for (int i = 1; i < n; i *= 10) {
+            length++;
+        }
+        return length;
+    }
+}
--- src/training2/SquareDegree.java
+++ src/training2/SquareDegree.java
@@ -9,10 +9,12 @@ public class SquareDegree {
         Scanner s = new Scanner(System.in);
         System.out.println("Which number do you want to square?");
         int a = s.nextInt();
+        double summary;
 
         if (a == 0) {
             System.out.println("No sense, input again");
             a = s.nextInt();
+
             if (a == 0) {
                 System.out.println("Always zero");
                 System.exit(0);
@@ -29,36 +31,37 @@ public class SquareDegree {
         }
 
         System.out.println("How many degrees?");
-
         int b = s.nextInt();
-        double summary = square(a, b);
-        if (summary < 1 && summary > 0) {
-            System.out.printf("It is " + "%.10f\n", summary);
-        } else {
-            System.out.printf("It is " + "%.1f\n", summary);
+        summary = square(a, b);
+        System.out.println(summary);
+    }
+
+    static int makePositive(int b) {
+        if (b < 0) {
+            return -1 * b;
         }
+        return b;
     }
 
     static double square(int a, int b) {
+        long result = a;
 
-        if (a > 10 || a < -10 || b > 10 || b < -10) {
+        for (int i = 1; i < makePositive(b); i++) {
+            result *= a;
+        }
+
+        if (a > 10 || a < -10) {
             return -1;
         } else if (a < 0) {
             return -2;
+        } else if (b > 10 || b < -10) {
+            return -1;
         } else if (b == 0) {
             return 1;
         } else if (b < 0) {
-            long result = a;
-            for (int i = b + 1; i < 0; i++) {
-                result *= a;
-            }
             return (double) 1 / result;
         }
 
-        long result = a;
-        for (int i = 1; i < b; i++) {
-            result *= a;
-        }
         return result;
     }
 }
