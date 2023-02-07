1e32d14c809249087de98201887bdb30e4f70b26
bakunts8 <khachikbakunts@gmail.com>
2023-02-07 14:06:27 +0400

    create a new function using a different solution method

--- src/training2/TextWord.java
+++ src/training2/TextWord.java
@@ -1,23 +1,23 @@
 package training2;
 public class TextWord {
     public static void main(String[] args) {
-        String text = "This Java tutorial covers basic to advanced concepts related to Java Programming";
+        String text = "This Java tutorial covers basic to advanced concepts related to Java Programming: String";
         String word = "java";
-        int count = compare(text,word);
-        System.out.println(count);
+        System.out.println(compare(text, word));   // Using compareTo
+        System.out.println(checkString(text));     // Using equals
+
     }
+
     static int compare(String t, String w) {
         int count = 0;
-        String t1 = t.toLowerCase();
-        String w1 = w.toLowerCase();
-        String[] str = t1.split(" ");
+        String[] str = t.split(" ");
         for (String s : str) {
-            boolean value = s.contains(w1);
-            if (value) {
+            int value = s.compareToIgnoreCase(w);
+            if (value == 0) {
                 count++;
             }
         }
-        if (w1.isEmpty()) {
+        if (w.isEmpty() || t.isEmpty()) {
             return -2;
         } else if (count == 0) {
             return -1;
@@ -25,22 +25,22 @@ public class TextWord {
         return count;
     }
 
-    //Example two.................
+    static int checkString(String t) {
+        int count = 0;
+        String word = "string";
+        String[] str = t.split(" ");
+        for (String s : str) {
+            boolean value = s.equalsIgnoreCase(word);
+            if (value) {
+                count++;
+            }
+        }
+        if (t.isEmpty() || word.isEmpty()) {
+            return -2;
+        } else if (count == 0) {
+            return -1;
+        }
+        return count;
+    }
 
-//    static int compare(String t, String w) {
-//        int count = 0;
-//        String[] str = t.split(" ");
-//        for (String s : str) {
-//            int value = s.compareToIgnoreCase(w);
-//            if (value == 0) {
-//                count++;
-//            }
-//        }
-//        if (w.isEmpty()) {
-//            return -2;
-//        } else if (count == 0) {
-//            return -1;
-//        }
-//        return count;
-//    }
 }
