409e740a1f529b5905cb56ef41af6ea563518e24
bakunts8 <khachikbakunts@gmail.com>
2023-02-24 00:52:22 +0400

    Add example two for sorted array

--- src/Part2/SortedArray.java
+++ src/Part2/SortedArray.java
@@ -23,5 +23,26 @@ public class SortedArray {
         for (int i : array) {
             System.out.print(i + " ");
         }
+
+//        //  if we know for sure that there are exactly these numbers in the array
+//
+//        int[] numbers = {-23, 5, 12};
+//        int[] array = {5, 12, -23, -23, 12, 5, 5, 12, -23, 12, -23, 5, -23, 12};
+//        int[] increaseArray = new int[array.length];
+//        int index = -1;
+//
+//        for (int num : numbers) {
+//
+//            for (int i : array) {
+//                if (i == num) {
+//                    index++;
+//                    increaseArray[index] = num;
+//                }
+//            }
+//        }
+//
+//        for (int sort : increaseArray) {
+//            System.out.print(sort + " ");
+//        }
     }
 }
